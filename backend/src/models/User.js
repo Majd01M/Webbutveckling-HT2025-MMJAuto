d1b0783407e86166102f78c422e64682af3451cd
Mo-Morad <momo22mi@student.ju.se>
2025-09-11 11:45:37 +0200

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    passwordHash: { type: String, required: true },
+    role: { type: String, enum: ['customer', 'admin'], default: 'customer' }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('User', UserSchema)
 
