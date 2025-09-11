b1240c7185cadad5069e157631f831cca901462d
Mo-Morad <momo22mi@student.ju.se>
2025-09-11 11:08:34 +0200

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import http from 'http'
+import app from './app.js'
+
+const PORT = process.env.PORT || 4000
+http.createServer(app).listen(PORT, () => {
+  console.log(`MMJAuto backend running at http://localhost:${PORT}`)
+})
 
