8eb116de40e89b57a2204493a66e6012586140e6
Mo-Morad <momo22mi@student.ju.se>
2025-09-15 18:17:01 +0200

    Update wishlistController.js

--- backend/src/controllers/wishlistController.js
+++ backend/src/controllers/wishlistController.js
@@ -1,1 +1,29 @@
+import Wishlist from '../models/Wishlist.js'
+
+export const getWishlist = async (req, res) => {
+  const wishlist = await Wishlist.findOne({ user: req.user.id }).populate('parts')
+  res.json(wishlist || { user: req.user.id, parts: [] })
+}
+
+export const addToWishlist = async (req, res) => {
+  const { partId } = req.body
+  let wishlist = await Wishlist.findOne({ user: req.user.id })
+  if (!wishlist) wishlist = await Wishlist.create({ user: req.user.id, parts: [] })
+
+  if (!wishlist.parts.includes(partId)) {
+    wishlist.parts.push(partId)
+    await wishlist.save()
+  }
+  res.json(wishlist)
+}
+
+export const removeFromWishlist = async (req, res) => {
+  const { partId } = req.params
+  const wishlist = await Wishlist.findOne({ user: req.user.id })
+  if (!wishlist) return res.status(404).json({ message: 'Wishlist not found' })
+
+  wishlist.parts = wishlist.parts.filter((id) => id.toString() !== partId)
+  await wishlist.save()
+  res.json(wishlist)
+}
 
