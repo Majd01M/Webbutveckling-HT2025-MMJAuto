ae03397800de569e11712a00c33602f07157aa09
Mo-Morad <momo22mi@student.ju.se>
2025-09-15 18:02:54 +0200

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
   const uri = process.env.MONGO_URI
-  if (!uri) throw new Error('MONGO_URI missing')
+  if (!uri) throw new Error('MONGO_URI missing in environment')
   await mongoose.connect(uri)
   console.log('✅ MongoDB connected')
 }
-
