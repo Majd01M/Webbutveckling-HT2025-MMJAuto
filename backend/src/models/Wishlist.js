5da53ed96f6b997cac22e05d77ee1cb4fe35259e
Mo-Morad <momo22mi@student.ju.se>
2025-09-15 18:15:40 +0200

    Update Wishlist.js

--- backend/src/models/Wishlist.js
+++ backend/src/models/Wishlist.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose'
+
+const WishlistSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    parts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Part' }]
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model('Wishlist', WishlistSchema)
 
