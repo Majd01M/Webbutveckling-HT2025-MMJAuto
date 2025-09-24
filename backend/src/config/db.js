bd350f588d57b80add5153636e91af9a9fba8e35
Mo-Morad <momo22mi@student.ju.se>
2025-09-24 17:13:03 +0200

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,1 +1,26 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mem;
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    let connStr = uri;
+
+    if (!uri || uri === "MEMORY") {
+      mem = await MongoMemoryServer.create();
+      connStr = mem.getUri("mmjauto");
+      console.log("Using in-memory MongoDB for development.");
+    }
+
+    const conn = await mongoose.connect(connStr);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
 
