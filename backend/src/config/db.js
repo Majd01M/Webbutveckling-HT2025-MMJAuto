dfbc7357933ddb34211a220eac4a246d496a9bf6
Majd01M <moma22wn@student.ju.se>
2025-09-24 14:28:42 +0200

    Delete backend directory

--- backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export const connectDB = async () => {
-  const uri = process.env.MONGO_URI
-  if (!uri) throw new Error('MONGO_URI missing in environment')
-  await mongoose.connect(uri)
-  console.log('✅ MongoDB connected')
-}
